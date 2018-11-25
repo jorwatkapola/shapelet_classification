481de8eb6a363a3d72a722a8255805897ffa2bc7
Jakub Orwat-Kapola <jkok1g14@soton.ac.uk>
2018-11-25 18:39:08 +0000

    Version 1 of the shapelet algorithm

---
+++ shapelet_main.py
@@ -0,0 +1,184 @@
+import sys
+sys.stdout.flush()
+import os
+import fnmatch
+import numpy as np
+from astropy.io import fits
+import matplotlib.pyplot as plt
+import math
+import shapelets as sha
+from operator import itemgetter
+
+#create a belloni_files list with names of files holding Belloni classified light curves
+clean_belloni = open('1915Belloniclass_updated.dat')
+lines = clean_belloni.readlines()
+states = lines[0].split()
+belloni_clean = {}
+belloni_files=[]
+belloni_files_dict = {}
+for h,l in zip(states, lines[1:]):
+    belloni_clean[h] = l.split()
+    #state: obsID1, obsID2...
+for state, obs in belloni_clean.items():
+    file_list=[]
+    for obID in obs:
+        file_list.append(obID)
+        belloni_files.append(obID+"_std1_lc.txt")
+    belloni_files_dict[state] = file_list
+    #state: file1, file2...
+ob_state = {}
+for state, obs in belloni_files_dict.items():
+    if state == "chi1" or state == "chi2" or state == "chi3" or state == "chi4": state = "chi"
+    for ob in obs:
+        ob_state[ob] = state
+    #file1: state
+#xp10408013100_lc.txt classified as chi1 and chi4, xp20402011900_lc.txt as chi2 and chi2
+#del ob_state["10408-01-31-00{}".format(extension)] as long as training and test sets are checked for duplicates when appending, it should be ok to keep
+
+
+available = []
+pool=[]
+for root, dirnames, filenames in os.walk("/home/jkok1g14/Documents/GRS1915+105/data/Std1_PCU2"):
+    for filename in fnmatch.filter(filenames, "*_std1_lc.txt"):
+        available.append(filename)
+for ob, state in ob_state.items():
+    if ob+"_std1_lc.txt" in available:
+        pool.append(ob)        
+training_obs = []
+training_states = []
+test_obs = []
+test_states = []
+randomize = np.random.choice(list(ob_state.keys()), len(ob_state.keys()), replace=False)
+for ob in randomize:
+    state = ob_state["{}".format(ob)]
+    if state not in training_states:
+        if ob in pool:
+            training_obs.append(ob)
+            training_states.append(state)
+no_train = math.ceil(len(pool)*0.50)
+for ob in training_obs:
+    pool.remove("{}".format(ob))        
+for ob in randomize:
+    state = ob_state["{}".format(ob)]
+    if state not in test_states:
+        if ob in pool:
+            test_obs.append(ob)
+            test_states.append(state)
+for ob in test_obs:
+    pool.remove("{}".format(ob))
+remaining = int(no_train-len(training_obs))
+train_remain = np.random.choice(pool, size = remaining, replace=False)
+for ob in train_remain:
+    training_obs.append(ob)
+# test_obs = []
+for ob in pool:
+    if ob not in training_obs:
+        test_obs.append(ob)
+        
+        
+#create a list of arrays with time and counts for the set of Belloni classified observations
+lc_dirs=[]
+lcs=[]
+ids=[]
+#for root, dirnames, filenames in os.walk("/export/data/jakubok/GRS1915+105/Std1_PCU2"):
+for root, dirnames, filenames in os.walk("/home/jkok1g14/Documents/GRS1915+105/data/Std1_PCU2"):    
+    for filename in fnmatch.filter(filenames, "*_std1_lc.txt"):
+        if filename in belloni_files:
+            lc_dirs.append(os.path.join(root, filename))
+
+            
+#make 2D arrays for light curves, with columns of counts and time values
+for lc in lc_dirs:
+    ids.append(lc.split("/")[-1].split("_")[0])
+    f=np.loadtxt(lc)
+    f=np.transpose(f)#,axis=1)
+    f=f[0:2]
+    ###1s average and time check to eliminate points outside of GTIs
+    f8t = np.mean(f[0][:(len(f[0])//8)*8].reshape(-1, 8), axis=1)
+    f8c = np.mean(f[1][:(len(f[1])//8)*8].reshape(-1, 8), axis=1)
+    rm_points = []
+    skip=False
+    for i in range(len(f8t)-1):
+        if skip==True:
+            skip=False
+            continue
+        delta = f8t[i+1]-f8t[i]
+        if delta > 1.0:
+            rm_points.append(i+1)
+            skip=True
+            
+####### normalise the count rates! think about the effect of 0-1 normalisation on the distance calculation
+            
+    times=np.delete(f8t,rm_points)
+    counts=np.delete(f8c,rm_points)
+    lcs.append(np.stack((times,counts)))
+    break
+print("No. of light curves prepared: {}".format(len(lcs)))
+
+
+
+
+
+
+
+
+
+
+
+
+
+#create a pool of shapelets for the set of light curves, best_shapelets contains one shapelet from every light curve that produced the greatest information gain
+tested_classes=[]
+best_shapelets=[]
+time_res=1
+for n_donor, lc_donor in enumerate(lcs):
+    #Create lists with classifications of all time-series relative to the donor time series; one that the pool of shapelets is generated from
+    state_donor = ob_state[ids[n_donor]]
+    if state_donor not in tested_classes:
+        tested_classes.append(state_donor)
+    else:
+        continue
+    belong_class=[]
+    other_class=[]
+    for n_lc in range(len(lcs)):
+        if ob_state[ids[n_lc]] == state_donor:
+            belong_class.append(n_lc)
+        else:
+            other_class.append(n_lc)
+    #calculate the entropy of the entire set, so it can be compared to the split set later
+    prop_belong = len(belong_class)/(len(belong_class)+len(other_class))
+    set_entropy = -(prop_belong)*math.log2(prop_belong)-(1-prop_belong)*math.log2(1-prop_belong)
+    pool=sha.generate_shapelets(lc_donor, 1, len(lc_donor[0]))#generate shapelets from the donor time-series, 
+    best_gain=0#set the initial best value of information gain to 0 (improved by any split) 
+    for shapelet in pool:
+        skip_shapelet=False#for entropy pruning
+        #set the order of distance calculations
+        #pick an other_class object first and then alternate between belong and other, when one group runs out, append the rest of the other group to the end
+        order=[]
+        if len(belong_class)<len(other_class):alternations=len(belong_class);larger_group=other_class
+        else: alternations=len(other_class); larger_group=belong_class
+        for i in range(alternations):
+            order.append(other_class[i])
+            order.append(belong_class[i])
+        for i in range(len(larger_group)-alternations):
+            order.append(larger_group[-(i+1)])
+        #start distance calculations
+        distances=[]
+        for n_lc in order:
+            lc=lcs[n_lc]
+            distance=sha.distance_calculation(n_lc, lc, shapelet, time_res, belong_class)
+           # print(n_donor, distance, shapelet)
+            distances.append(distance)
+            if len(distances)>1:
+                best_split=sha.best_split_point(distances, set_entropy)
+                skip_shapelet=sha.entropy_pruning(best_gain, distances, best_split, len(belong_class), len(other_class), set_entropy)
+                if skip_shapelet==True:
+                    break
+        if skip_shapelet==False:
+            gain=sha.information_gain(distances, set_entropy, best_split)
+            #print(shapelet)
+            #print(distances)
+            if gain>best_gain:
+                best_gain=gain
+                best_shapelet=shapelet
+    best_shapelets.append((best_shapelet, best_gain, state_donor))
---
+++ shapelets.py
@@ -0,0 +1,104 @@
+import numpy as np
+import math
+from operator import itemgetter
+
+def generate_shapelets(light_curve, minlen, maxlen, time_res=1.):
+    """Create a list of all possible subsegments from light_curve, which is a 2D array, where [0,:] are the time values in seconds, and [1,:] are the count rate values. minlen and maxlen are the lower and upper limits of subsegment length in the unit of seconds. Output is a list of 1D arrays.The shortest shapelet will be an array of length minlen+1 etc. time_res is the time interval between two data points in seconds, so that for time_res=0.5 and minlen=100, the smallest produced shapelet would be an array of length (100/0.5)+1. Thus time_res should be set to a value that produces an integer.
+    """
+    pool=[]
+    lc = light_curve
+    minlen=int(minlen/time_res); maxlen=int(maxlen/time_res)
+    for l in range(minlen,maxlen+1):
+        end=l+1; start=0
+        while end<=len(lc[0]):
+            sh=lc[1,start:end]
+            inter=(lc[0,end-1]-lc[0,start])
+            end+=1; start+=1
+            if inter/time_res==float(l):
+                pool.append(sh)
+    return pool
+
+def information_gain(distances, set_entropy, split_point):
+    """Calculate the information gain from splitting the dichotomous set of time-series objects into subsets below and above the split_point, depending on their distances to the tested shapelet. "distances" is a list of tuples, where each tuple corresponds to a time-series object, and distances[i][0] is the id number of the ith object, distances[i][1] is the minimal distance between the shapelet and the object, distances[i][2] is equal to 1 if the object belongs to the tested object class, or to 0 otherwise (other_class objects). Information gain is dependent on the entropy of the entire set and the split set, where entropy is I(set)=-Alog2(A)-Blog2(B) (A=proportion of object in the set that belong to the class, B=proportion of other objects). Information gain is then I(set)-
+    """
+    if split_point !=0 and split_point !=np.inf:
+        above=[lc for lc in distances if lc[1]>=split_point]
+        above_belong=sum([lc[2] for lc in above])
+        below=[lc for lc in distances if lc[1]<split_point]
+        below_belong=sum([lc[2] for lc in below])
+        prop_above_belong=above_belong/len(above)
+        prop_below_belong=below_belong/len(below)
+        if prop_above_belong==1. or prop_above_belong==0.:
+            above_entropy=0
+        else:
+            above_entropy = -(prop_above_belong)*math.log2(prop_above_belong)-(1-prop_above_belong)*math.log2(1-prop_above_belong)
+        if prop_below_belong==1. or prop_below_belong==0.:
+            below_entropy =0
+        else:
+            below_entropy = -(prop_below_belong)*math.log2(prop_below_belong)-(1-prop_below_belong)*math.log2(1-prop_below_belong)
+        return set_entropy-(len(above)/(len(above)+len(below)))*(above_entropy)-(len(below)/(len(above)+len(below)))*(below_entropy)
+    else:
+        return "Invalid split point (0 or infinity)."
+
+def distance_calculation(n_lc, lc, shapelet, time_res, belong_class):
+    """
+    """
+    best_dist=np.inf #for "early abandon"
+    lc_l = len(lc[0])
+    sha_l=len(shapelet)
+    for start_p in range(lc_l-sha_l+1):#length difference+1 will give the number of iterations required to shift the moving windown from start to end of the LC (with a difference of one point, two window positions are required etc.)
+        end_p=start_p+sha_l-1 #-1 to give the index of the last included point
+        if lc[0,end_p]-lc[0,start_p] != (sha_l-1)*time_res:
+            continue
+        skip=False
+        sha_dist=0 #distance between shapelet and LC subsegment
+        for i in range(sha_l):
+            sha_dist += (lc[1,i+start_p]-shapelet[i])**2
+            if sha_dist>=best_dist: 
+                skip=True#"early abandon"
+                break#break out of the distance calculation and skip the position of the moving window
+        if skip ==False:
+            best_dist=sha_dist
+    if n_lc in belong_class:
+        class_assign=1
+    else:
+        class_assign=0
+    return (n_lc, best_dist, class_assign)
+    
+def best_split_point(distances, set_entropy):
+    """
+    """
+    distances.sort(key=itemgetter(1))
+    best_gain_split=0
+    best_split=0
+    for distance in range(len(distances)-1):
+        split_point=(distances[distance][1] + distances[distance+1][1])/2
+        gain=information_gain(distances, set_entropy, split_point)
+        if isinstance(gain, str) == True:
+            continue
+        if gain>best_gain_split:
+            best_gain_split=gain
+            best_split=split_point
+    return best_split
+
+def entropy_pruning(best_gain, distances, best_split, belong_class_count, other_class_count, set_entropy):
+    """
+    """
+    calc_belong=sum([lc[2] for lc in distances])
+    calc_other=len(distances)-calc_belong
+    distances_bcs=distances #best case scenario when all the distances are included
+    distances_bcs.sort(key=itemgetter(1))
+    maxdist=distances_bcs[-1][1]+1
+    for add_belong in range(belong_class_count-calc_belong):
+        distances_bcs.append((-1,0,1))
+    for add_other in range(other_class_count-calc_other):
+        distances_bcs.append((-1,maxdist,0))
+    gain_bcs=information_gain(distances_bcs, set_entropy, best_split)
+    if isinstance(gain_bcs, str) == True:
+        return True
+    else:
+        if gain_bcs<best_gain:
+            return True
+        else:
+            return False
+    
